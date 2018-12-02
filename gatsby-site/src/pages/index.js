9703bd699dc168fa67070f73c614122e2d7923b3
Haley Sulcer <hjsulcer@gmail.com>
2018-12-02 17:19:26 -0500

    added photo site to Projects and turned Education section into a table

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import './styles.css'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
 
   <Layout>
     <h1>Hello!</h1>
-    <p id='#example'>I'm a Frontend Developer based in NY and NJ.</p>
-    <p>I'm also a skilled tennis player and an avid  <a href="https://500px.com/hjs342" target="_blank" rel="noopener noreferrer">street photographer.</a></p>
+    <p id='#example'>I'm a web developer based in NY and NJ.</p>
+    <p>I'm also a skilled tennis player and an avid  <a href="https://haleysulcerphoto.netlify.com" target="_blank" rel="noopener noreferrer">street photographer.</a></p>
 
     <h3>Education</h3>
-      <p><strong><a href="https://generalassemb.ly/" target="_blank" rel="noopener noreferrer">General Assembly</a></strong> Web Development Immersive - May 2018</p>
 
-      <p><strong><a href="https://tcnj.pages.tcnj.edu/" target="_blank" rel="noopener noreferrer">The College of New Jersey</a></strong> B.A. in Public & Mass Communication - May 2015</p>
+    <table className="edu-table">
+      <tr>
+        <td><strong><a href="https://generalassemb.ly/" target="_blank" rel="noopener noreferrer">General Assembly</a></strong></td>
+        <td>Web Development Immsersive</td>
+        <td>May 2018</td>
+      </tr>
 
-      <p><strong><a href="https://www.ucc.edu/" target="_blank" rel="noopener noreferrer">Union County College</a></strong> A.A. in Liberal Arts - May 2013</p>
+      <tr>
+        <td><strong><a href="https://tcnj.pages.tcnj.edu/" target="_blank" rel="noopener noreferrer">The College of New Jersey</a></strong></td>
+        <td>B.A. in Public & Mass Communication</td>
+        <td>May 2015</td>
+      </tr>
 
+      <tr>
+        <td><strong><a href="https://www.ucc.edu/" target="_blank" rel="noopener noreferrer">General Assembly</a></strong></td>
+        <td>A.A. in Liberal Arts</td>
+        <td>May 2013</td>
+      </tr>
+    </table>
+
+  
     <Link to="/projects-tech/">Projects and Tech</Link>
     <Link to ="/contact/">Contact</Link>
   </Layout>
--- gatsby-site/src/pages/projects-tech.js
+++ gatsby-site/src/pages/projects-tech.js
@@ -14,13 +14,16 @@ const ProjectsTech = () => (
         <p>PC Wizard helps people build computers. Users can view information on different types of computer components, as well as information regarding specific computer parts (series, price, RAM slots, etc). <strong>Built with:</strong> Rails, Devise, and Bulma. <a href="https://github.com/Tsardines/pc-wizard" target="_blank" rel="noopener noreferrer">Github</a></p>
 
       <h3><a href="https://spark-lines.herokuapp.com/" target="_blank" rel="noopener noreferrer">Spark Lines</a></h3>
-      <p>Spark Lines is a single-page news aggregator. Users can also submit their own articles. <strong>Built with:</strong> Node, Express, PostgreSQL, and NewsAPI. <a href="https://github.com/Tsardines/news-app" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <p>Spark Lines is a single-page news aggregator. Users can also submit their own articles. <strong>Built with:</strong> Node, Express, PostgreSQL, and NewsAPI. <a href="https://github.com/Tsardines/news-app" target="_blank" rel="noopener noreferrer">Github</a></p>
 
       <h3><a href="https://terminal-hacker.netlify.com/" target="_blank" rel="noopener noreferrer">Terminal Hacker</a></h3>
-      <p>This is a recreation of the terminal hacking mini game from the Fallout series. <strong>Built with:</strong> HTML, CSS, and JavaScript. <a href="https://github.com/Tsardines/fallout-terminal" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <p>This is a recreation of the terminal hacking mini game from the Fallout series. <strong>Built with:</strong> HTML, CSS, and JavaScript. <a href="https://github.com/Tsardines/fallout-terminal" target="_blank" rel="noopener noreferrer">Github</a></p>
 
       <h3><a href="https://afternoon-citadel-73979.herokuapp.com" target="_blank" rel="noopener noreferrer">Marvel Character Finder</a></h3>
-      <p>Allows users to create accounts, view information on over a thousand characters, and add characters to their favorites list. <strong>Built with:</strong> Marvel API, React, Express, Node, PostgreSQL, and Bootstrap. <a href="https://github.com/Tsardines/MarvelApp" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <p>Allows users to create accounts, view information on over a thousand characters, and add characters to their favorites list. <strong>Built with:</strong> Marvel API, React, Express, Node, PostgreSQL, and Bootstrap. <a href="https://github.com/Tsardines/MarvelApp" target="_blank" rel="noopener noreferrer">Github</a></p>
+
+      <h3><a href="https://haleysulcerphoto.netlify.com" target="_blank" rel="noopener noreferrer">Photography Portfolio</a></h3>
+        <p>I built a site that showcases my photography. <strong>Built with:</strong> React, TypeIt, and react-photo-gallery. <a href="https://github.com/Tsardines/react-photo" target="_blank" rel="noopener noreferrer">Github</a></p>
 
 
 
