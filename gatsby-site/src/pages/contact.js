64fe26f5b7420d51ac3bb51295253604762c747d
Haley Sulcer <hjsulcer@gmail.com>
2018-12-25 13:43:59 -0500

    added Exp section to home and fixed a spelling error in Education table...also changed color scheme

--- gatsby-site/src/pages/contact.js
+++ gatsby-site/src/pages/contact.js
@@ -9,7 +9,7 @@ const Contact = () => (
       <h4><a href="mailto:hjsulcer@gmail.com" target="_blank" rel="noopener noreferrer">hjsulcer@gmail.com</a></h4>
       <h4><a href="https://github.com/Tsardines" target="_blank" rel="noopener noreferrer">GitHub</a></h4>
       <h4><a href="https://www.linkedin.com/in/haley-sulcer/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h4>
-      <h4><a href="https://500px.com/hjs342" target="_blank" rel="noopener noreferrer">Photography</a></h4>
+      <h4><a href="https://haleysulcerphoto.netlify.com" target="_blank" rel="noopener noreferrer">Photography</a></h4>
     <Link to="/">Home</Link>
     <Link to="/projects-tech/">Projects and Tech</Link>
   </Layout>
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -11,12 +11,33 @@ const IndexPage = () => (
     <p id='#example'>I'm a web developer based in NY and NJ.</p>
     <p>I'm also a skilled tennis player and an avid  <a href="https://haleysulcerphoto.netlify.com" target="_blank" rel="noopener noreferrer">street photographer.</a></p>
 
+    <br />
+
+    <h3>Experience</h3>
+
+    <table className="work-table">
+
+      {/* <tr>Junior Web Developer</tr>
+      <tr><a href="https://kobrekim.com/">Kobre & Kim</a></tr>
+      <tr>New York, NY</tr>
+      <tr>January 2019 - Present</tr> */}
+      <tr>
+        <td>Junior Developer</td>
+        <td><a href="https://kobrekim.com/">Kobre & Kim</a></td>
+        <td>New York, NY</td>
+        <td>January 2019 - Present</td>
+      </tr>
+
+    </table>
+
+    <br />
+
     <h3>Education</h3>
 
     <table className="edu-table">
       <tr>
         <td><strong><a href="https://generalassemb.ly/" target="_blank" rel="noopener noreferrer">General Assembly</a></strong></td>
-        <td>Web Development Immsersive</td>
+        <td>Web Development Immersive</td>
         <td>May 2018</td>
       </tr>
 
